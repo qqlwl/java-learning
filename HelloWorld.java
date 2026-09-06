a8691dd972014d58297965edcf6dd18fd3437c70
qqlwl <325652077+qqlwl@users.noreply.github.com>
2026-09-06 21:56:36 +0800

    feat: 第二次提交,HelloWorld 加入个人目标与学习日志

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("我的 Java 之路,从今天开始。");
+        System.out.println("我是 lwl,2026 年 9 月 6 日,我的 Java 之路正式开始。");
+        System.out.println("目标:毕业前成为能独立干活的 Java 工程师。");
     }
 }
